885fb930df870e579d7a3147da201b0237bd5ca9
Lucas Vieira <vieira07lucas@gmail.com>
2022-12-08 13:47:37 -0300

    Particionando o HTML

--- view/cursos/formulario.php
+++ view/cursos/formulario.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="container">
-        <div class="jumbotron">
-            <h1>Novo Curso</h1>
-        </div>
+<?php include __DIR__ . '/../inicio-html.php'; ?>
 
         <form action="">
             <div class="form-group">
@@ -22,7 +9,5 @@
                 Salvar
             </button>
         </form>
-    </div>
-</body>
 
-</html>
+<?php include __DIR__ . '/../fim-html.php'; ?>
