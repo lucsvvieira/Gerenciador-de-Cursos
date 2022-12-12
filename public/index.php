b1fe36a312585ebd43473ca02e17550b1d510683
Lucas Vieira <vieira07lucas@gmail.com>
2022-12-12 11:02:50 -0300

    Usando Rotas

--- public/index.php
+++ public/index.php
@@ -6,20 +6,14 @@ use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
 
-    switch ($_SERVER['PATH_INFO']) {
-        case '/listar-cursos':
-            $controlador = new ListarCursos();
-            $controlador->processaRequisicao();
-            break;
-        case '/novo-curso':
-            $controlador = new FormularioInsercao();
-            $controlador->processaRequisicao();
-            break;
-        case '/salvar-curso':
-            $controlador = new Persistencia();
-            $controlador->processaRequisicao();
-            break;
-        default:
-            echo "Erro 404";
-            break;
-    }
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
+
+if (!array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
+}
+
+$classeControladora = $rotas[$caminho];
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
